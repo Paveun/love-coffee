9032661697101e2798e46926a6def0d9e24d2fdf
Paveun <atresierrajansen@gmail.com>
2024-09-06 21:40:50 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import Wave from 'react-wavify';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <FontAwesomeIcon icon={faCoffee} className="coffee-icon" />
+        <h1>paveun.</h1>
+      </div>
+
+      <div className="wave-container">
+        <Wave
+          fill="#d2b09b"
+          paused={false}
+          options={{
+            height: 60,
+            amplitude: 50,
+            speed: 0.2,
+            points: 4,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
